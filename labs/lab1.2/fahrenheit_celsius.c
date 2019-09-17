91d5d4da1e426e748ad87bd5763976cd1d721ec2
Eduardo Méndez <eduardomndez@ymail.com>
2019-09-17 17:20:00 -0500

    Word Reverser Incompleto

---
+++ labs/lab1.2/fahrenheit_celsius.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* print Fahrenheit-Celsius table */
+
+int main(int argc, char **argv)
+{
+  if (argc == 2){
+    printf("Fahrenheit: %3d, Celcius: %6.1f\n", atoi(argv[1]), (5.0/9.0)*(atoi(argv[1])-32));
+
+
+  } else if (argc == 4){
+    int fahr, start, end, step;
+    start = atoi(argv[1]);
+    end = atoi(argv[2]);
+    step = atoi(argv[3]);
+
+    for (fahr = start; fahr <= end; fahr = fahr + step)
+    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+
+
+  }
+
+    return 0;
+}
--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -2,5 +2,21 @@
 
 int main(){
     // Place your magic here
-    return 0;
+   char word[100];
+   printf("Enter your name: ");
+   /*fgets(word, 100, stdin);
+   printf("Your Name is: %s",word);
+   */
+
+   /*
+   while(scanf("%[^\n]s",word) != EOF){
+     printf("Your Name is: %s",word);
+   }*/
+
+   while(fgets(word, 100, stdin)!=EOF){
+     printf("Your Name is: %s",word);
+     printf("Enter your name: ");
+     word = NULL;
+   }
+   return 0;
 }
