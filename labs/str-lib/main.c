9f474b6a0f126fcb284178735ddce1a90fc2ca28
Eduardo Méndez <eduardomndez@ymail.com>
2019-09-17 20:38:11 -0500

    Lab Str - Not 100 functional

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char **argv) {
+
+    char * origin = (char*)malloc(100);
+    char * addition = (char*)malloc(100);
+
+    origin = argv[1];
+    addition = argv[2];
+
+
+    printf("Initial Lenght       : %d\n", mystrlen(origin));
+    mystradd(origin,addition);
+    printf("New String           : %s\n", origin);
+
+    if(mystrfind(origin,argv[3])){
+      printf("SubString was found  : yes\n");
+    } else{
+      printf("SubString was found  : no\n");
+    }
+
+
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,43 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i, len = 0;
+    for (i = 0; str[i] != '\0'; i++){
+      len++;
+    }
+
+    return len;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+void mystradd(char * origin, char * addition){
+
+  int i, j;
+  i =mystrlen(origin);
+  for (j=0;addition[j]!='\0';j++){
+    i = i +j;
+    origin[i]=addition[j];
+  }
+  //origin[mystrlen(origin)-1]='\0';
+
 }
 
 int mystrfind(char *origin, char *substr){
+
+  int i, j, lenO, lenS;
+
+  lenO = mystrlen(origin);
+  lenS = mystrlen(substr);
+
+  for (i = 0; i<lenO; i++){
+    for (j = 0; j<lenS; j++){
+      if(origin[i+j]!=substr[j]){
+        break;
+      }
+      if(j==lenS-1){
+        return 1;
+      }
+    }
+  }
+
     return 0;
 }
